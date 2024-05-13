31282e8944b437fc71a4e31d41fb139333a4182a
gileredman <75474928+gileredman@users.noreply.github.com>
2024-05-13 17:55:47 +0700

    Update and rename k.sh to upcloudwin.sh

---
+++ upcloudwin.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+echo "请选择您需要的镜像包:"
+echo "  1) Windows 2012"
+echo "  2) Windows 2016"
+echo "  3) Windows 2019"
+echo "  4) Windows 2022"
+echo "  5) Windows 10"
+echo "  6) Windows 11"
+echo ""
+echo -n "请输入编号: "
+read N
+case $N in
+  1) wget -O- 'https://bit.ly/WIN12XZ' | xzcat | dd of=/dev/vda;;
+  2) wget -O- 'https://bit.ly/WIN16XZ' | xzcat | dd of=/dev/vdb ;;
+  3) wget -O- 'https://bit.ly/WIN19XZ' | xzcat | dd of=/dev/vda ;;
+  4) wget -O- 'https://bit.ly/WIN22XZ' | xzcat | dd of=/dev/sda ;;
+  5) wget -O- 'https://bit.ly/WIN10XZ' | xzcat | dd of=/dev/vdb ;;
+  6) wget -O- 'https://bit.ly/4afmzIk' | xzcat | dd of=/dev/sda ;;
+  *) echo "Wrong input!" ;;
+esac 
