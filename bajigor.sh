b591ac0be714684c6bc60cad377588a00bf32853
gileredman <75474928+gileredman@users.noreply.github.com>
2024-07-17 21:27:02 +0700

    Update bajigor.sh

--- bajigor.sh
+++ bajigor.sh
@@ -10,11 +10,11 @@ echo ""
 echo -n "请输入编号: "
 read N
 case $N in
-  1) wget -qO- inst.sh|bash -s - -t https://bit.ly/3VAINAe ;;
-  2) wget -qO- inst.sh|bash -s - -t https://bit.ly/WIN16GZ ;;
-  3) wget -qO- inst.sh|bash -s - -t https://bit.ly/WIN19GZ ;;
-  4) wget -qO- inst.sh|bash -s - -t https://bit.ly/WIN22GZ ;;
-  5) wget -qO- inst.sh|bash -s - -t https://bit.ly/WIN10GZ ;;
-  6) wget -qO- inst.sh|bash -s - -t https://bit.ly/WIN11GZ ;;
+  1) wget -q https://raw.githubusercontent.com/bin456789/reinstall/main/reinstall.sh && bash reinstall.sh dd --img=https://www.dropbox.com/scl/fi/v1tjgtma9uzvqac2o958b/windows2012.gz?rlkey=ix61un5ecqlexy93d50vmw1t3&st=6dx3pgwn&dl=0 ;;
+  2) wget -q https://raw.githubusercontent.com/bin456789/reinstall/main/reinstall.sh && bash reinstall.sh dd --img=https://www.dropbox.com/scl/fi/faclvht7wzsc6dk21kogf/windows2016.gz?rlkey=gikoin3l4s5ksna50j6w5gknd&st=7jpjgwq6&dl=0 ;;
+  3) wget -q https://raw.githubusercontent.com/bin456789/reinstall/main/reinstall.sh && bash reinstall.sh dd --img=https://www.dropbox.com/scl/fi/jtx0yonyt2wv333f5zwtd/windows2019.gz?rlkey=kkmgadopud8dqdv5cwra0eis6&st=virxlt1h&dl=0 ;;
+  4) wget -q https://raw.githubusercontent.com/bin456789/reinstall/main/reinstall.sh && bash reinstall.sh dd --img=https://www.dropbox.com/scl/fi/37zlcd08ccyplppu1d6v4/windows2022.gz?rlkey=gykmr6dudursvklpxdsz4if26&st=87xxhf7c&dl=0 ;;
+  5) wget -q https://raw.githubusercontent.com/bin456789/reinstall/main/reinstall.sh && bash reinstall.sh dd --img=https://www.dropbox.com/scl/fi/pifgb3n74s26act0k449s/windows10.gz?rlkey=0vd8db2xa4yfsd0ubzsplo0bn&st=tvn590mw&dl=0 ;;
+  6) wget -q https://raw.githubusercontent.com/bin456789/reinstall/main/reinstall.sh && bash reinstall.sh dd --img=https://www.dropbox.com/scl/fi/5ntnmctersyjh2niajryl/windows11.gz?rlkey=zbfho4w49vovt8056g6uunuaa&st=aw6ews7g&dl=0 ;;
   *) echo "Wrong input!" ;;
 esac 
