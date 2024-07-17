8f2ca10c58b31fa53fee8eff890741db96810995
gileredman <75474928+gileredman@users.noreply.github.com>
2024-07-17 23:54:24 +0700

    Update lakone.sh

--- ginseng.sh
+++ ginseng.sh
@@ -1,82 +1,20 @@
-#!/bin/bash
-
-# Function to display menu and get user choice
-display_menu() {
-    echo "Please select the Windows Server version:"
-    echo "1. Windows Server 2012"
-    echo "2. Windows Server 2016"
-    echo "3. Windows Server 2019"
-    echo "4. Windows Server 2022"
-    echo "5. Windows 10"
-    echo "6. Windows 11"
-    read -p "Enter your choice: " choice
-}
-
-# Update package repositories and upgrade existing packages
-
-# Install QEMU and its utilities
-
-# Get user choice
-display_menu
-
-case $choice in
-   1)
-
-        # Windows Server 2012
-        img_file="windows2012.gz"
-        iso_link="https://www.dropbox.com/scl/fi/v1tjgtma9uzvqac2o958b/windows2012.gz?rlkey=ix61un5ecqlexy93d50vmw1t3&st=6dx3pgwn&dl=0"
-        iso_file="windows2012.iso"
-        ;;
-    2)
-
-        # Windows Server 2016
-        img_file="windows2016.gz"
-        iso_link="https://www.dropbox.com/scl/fi/faclvht7wzsc6dk21kogf/windows2016.gz?rlkey=gikoin3l4s5ksna50j6w5gknd&st=7jpjgwq6&dl=0"
-        iso_file="windows2016.iso"
-        ;;
-    3)
-        # Windows Server 2019
-        img_file="windows2019.gz"
-        iso_link="https://www.dropbox.com/scl/fi/jtx0yonyt2wv333f5zwtd/windows2019.gz?rlkey=kkmgadopud8dqdv5cwra0eis6&st=virxlt1h&dl=0"
-        iso_file="windows2019.iso"
-        ;;
-    4)
-        # Windows Server 2022
-        img_file="windows2022.gz"
-        iso_link="https://www.dropbox.com/scl/fi/37zlcd08ccyplppu1d6v4/windows2022.gz?rlkey=gykmr6dudursvklpxdsz4if26&st=87xxhf7c&dl=0"
-        iso_file="windows2022.iso"
-        ;;
-        5)
-        # Windows 10
-        img_file="windows10.gz"
-        iso_link="https://www.dropbox.com/scl/fi/pifgb3n74s26act0k449s/windows10.gz?rlkey=0vd8db2xa4yfsd0ubzsplo0bn&st=tvn590mw&dl=0"
-        iso_file="windows10.iso"
-        ;;
-        6)
-        # Windows 11
-        img_file="windows11.gz"
-        iso_link="https://www.dropbox.com/scl/fi/5ntnmctersyjh2niajryl/windows11.gz?rlkey=zbfho4w49vovt8056g6uunuaa&st=aw6ews7g&dl=0"
-        iso_file="windows11.iso"
-        ;;
-    *)
-        echo "Invalid choice. Exiting."
-        exit 1
-        ;;
-esac
-
-echo "Selected Windows Server version: $img_file"
-
-# Create a raw image file with the chosen name
-
-sudo apt-get update && apt-get install  binutils -y
-
-echo "Image file $img_file created successfully."
-
-sudo apt-get install  binutils -y
-
-echo "Virtio driver ISO downloaded successfully."
-
-# Download Windows ISO with the chosen name
-wget -qO- https://raw.githubusercontent.com/gileredman/kiki/main/bangor.sh && bash bangor.sh|bash dd --img="$iso_link"
-
-echo "Windows ISO downloaded successfully."
+#!/bin/sh
+echo "MANUSIAASU"
+echo "  1) Windows 2012"
+echo "  2) Windows 2016"
+echo "  3) Windows 2019"
+echo "  4) Windows 2022"
+echo "  5) Windows 10"
+echo "  6) Windows 11"
+echo ""
+echo -n "请输入编号: "
+read N
+case $N in
+  1) wget -q https://raw.githubusercontent.com/gileredman/kiki/main/bangor.sh && bash bangor.sh dd --img=https://www.dropbox.com/scl/fi/v1tjgtma9uzvqac2o958b/windows2012.gz?rlkey=ix61un5ecqlexy93d50vmw1t3&st=6dx3pgwn&dl=0 ;;
+  2) wget -q https://raw.githubusercontent.com/gileredman/kiki/main/bangor.sh && bash bangor.sh dd --img=https://www.dropbox.com/scl/fi/faclvht7wzsc6dk21kogf/windows2016.gz?rlkey=gikoin3l4s5ksna50j6w5gknd&st=7jpjgwq6&dl=0 ;;
+  3) wget -q https://raw.githubusercontent.com/gileredman/kiki/main/bangor.sh && bash bangor.sh dd --img=https://www.dropbox.com/scl/fi/jtx0yonyt2wv333f5zwtd/windows2019.gz?rlkey=kkmgadopud8dqdv5cwra0eis6&st=virxlt1h&dl=0 ;;
+  4) wget -q https://raw.githubusercontent.com/gileredman/kiki/main/bangor.sh && bash bangor.sh dd --img=https://www.dropbox.com/scl/fi/37zlcd08ccyplppu1d6v4/windows2022.gz?rlkey=gykmr6dudursvklpxdsz4if26&st=87xxhf7c&dl=0 ;;
+  5) wget -q https://raw.githubusercontent.com/gileredman/kiki/main/bangor.sh && bash bangor.sh dd --img=https://www.dropbox.com/scl/fi/pifgb3n74s26act0k449s/windows10.gz?rlkey=0vd8db2xa4yfsd0ubzsplo0bn&st=tvn590mw&dl=0 ;;
+  6) wget -q https://raw.githubusercontent.com/gileredman/kiki/main/bangor.sh && bash bangor.sh dd --img=https://www.dropbox.com/scl/fi/5ntnmctersyjh2niajryl/windows11.gz?rlkey=zbfho4w49vovt8056g6uunuaa&st=aw6ews7g&dl=0 ;;
+  *) echo "Wrong input!" ;;
+esac 
