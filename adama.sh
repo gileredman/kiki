2857e0e3eb7d00d9cb201f4c039680028c47b307
gileredman <75474928+gileredman@users.noreply.github.com>
2025-12-05 22:51:20 +0700

    Update adama.sh

--- adama.sh
+++ adama.sh
@@ -11,7 +11,7 @@ TARGET_PART="${TARGET_DISK}1"
 
 ADMIN_PASS="P@ssw0rd123"                               # GANTI password Windows
 
-RESCUE_MARKER="/ayama/rescue"
+RESCUE_MARKER="/adama/rescue"
 INSTALLER_FLAG="/tmp/run_windows_installer"
 
 ### ===========================
@@ -20,7 +20,7 @@ INSTALLER_FLAG="/tmp/run_windows_installer"
 
 if [[ ! -f "$RESCUE_MARKER" ]]; then
     echo "== MODE NORMAL, masuk rescue =="
-    bash /root/ayama.sh --rescue     # PANGGIL SCRIPT RESCUE KAMU
+    bash /root/adama.sh --rescue     # PANGGIL SCRIPT RESCUE KAMU
     touch "$INSTALLER_FLAG"
     reboot
     exit 0
